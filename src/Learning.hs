34697c2b43fc9b593be4c0b84401944b118bc035
Bogdan <masterdezign@gmail.com>
2018-02-07 16:52:30 +0100

    Documentation

--- src/Learning.hs
+++ src/Learning.hs
@@ -1,6 +1,6 @@
 -- |
 -- = Machine learning utilities
--- 
+--
 -- A micro library containing the most common machine learning tools.
 -- Check also the mltool package https://hackage.haskell.org/package/mltool.
 
@@ -14,10 +14,12 @@ module Learning (
   , pca
 
   -- * Supervised learning
+  , Teacher
+  , teacher
   , Classifier
+  , Readout
   , learn
   , learn'
-  , teacher
   , scores
   , winnerTakesAll
 
@@ -29,7 +31,7 @@ module Learning (
 import           Numeric.LinearAlgebra
 import qualified Data.Vector.Storable as V
 
--- Supervised dataset
+-- | A dataset representation for supervised learning
 data Dataset a b = Dataset { _samples :: [a], _labels :: [b] }
 
 -- | Computes "covariance matrix", alternative to (snd. meanCov).
@@ -47,15 +49,19 @@ pca' maxDim xs = tr u ? [0..maxDim - 1]
     -- Eigenvectors matrix U
     (u, _, _) = svd $ unSym sigma
 
+-- | Principal components analysis tools
 data PCA = PCA
-  { _u :: Matrix Double  -- Compression matrix U
+  { _u :: Matrix Double
+  -- ^ Compression matrix U
   , _compress :: Vector Double -> Matrix Double
+  -- ^ Compression function
   , _decompress :: Matrix Double -> Vector Double
+  -- ^ Inverse to compression function
   }
 
 -- | Principal component analysis (PCA)
-pca :: Int
-    -> [Vector Double]
+pca :: Int  -- ^ Number of principal components to preserve
+    -> [Vector Double]  -- ^ Analyzed data samples
     -> PCA
 pca maxDim xs = let u' = pca' maxDim xs
                     u = tr u'
@@ -65,15 +71,31 @@ pca maxDim xs = let u' = pca' maxDim xs
                    , _decompress = flatten. (u <>)
                    }
 
-type Classifier a = (Matrix Double -> a)
+-- | Classifier function that maps some network state
+-- into a categorical output
+type Classifier a = Matrix Double -> a
 
--- | Perform supervised learning to create a linear classifier.
--- The ridge regression is run with regularization parameter mu=1e-4.
+-- | Linear readout (matrix)
+type Readout = Matrix Double
+
+-- | Teacher matrix
+--
+--     > 0 0 0 0 0
+--     > 0 0 0 0 0
+--     > 1 1 1 1 1 <- Desired class index is 2
+--     > 0 0 0 0 0 <- Number of classes is 4
+--     >         ^
+--     >   5 repetitions
+type Teacher = Matrix Double
+
+-- | Perform supervised learning (ridge regression) and create
+-- a linear classifier function.
+-- The regression is run with regularization parameter mu=1e-4.
 learn
-  :: V.Storable a =>
-     Vector a
-     -> Matrix Double
-     -> Matrix Double
+  :: (V.Storable a, Eq a) =>
+     Vector a  -- ^ All possible outcomes (classes) list
+     -> Matrix Double  -- ^ Network state (nonlinear response)
+     -> Matrix Double  -- ^ Horizontally concatenated `Teacher` matrices
      -> Either String (Classifier a)
 learn klasses xs teacher' =
   case learn' xs teacher' of
@@ -85,30 +107,37 @@ learn klasses xs teacher' =
      -> Matrix Double
      -> Either String (Classifier Int) #-}
 
--- | Create a linear readout using the ridge regression
+-- | Create a linear `Readout` using the ridge regression.
+-- Similar to @learn@, but instead of a `Classifier` function
+-- a (already transposed) `Readout` matrix may be returned.
 learn'
-  :: Matrix Double
-     -> Matrix Double
-     -> Maybe (Matrix Double)
+  :: Matrix Double  -- ^ Network state (nonlinear response)
+  -> Matrix Double  -- ^ Horizontally concatenated `Teacher` matrices
+  -> Maybe Readout
 learn' a b = case ridgeRegression 1e-4 a b of
     (Just x) -> Just (tr x)
     _ -> Nothing
 
--- | Teacher matrix
-teacher :: Int -> Int -> Int -> Matrix Double
+-- | Create a binary `Teacher` matrix with ones row corresponding to
+-- the desired class index
+teacher
+  :: Int  -- ^ Number of classes (labels)
+  -> Int  -- ^ Desired class index (starting from zero)
+  -> Int  -- ^ Number of repeated columns in teacher matrix
+  -> Teacher
 teacher nLabels correctIndex repeatNo = fromBlocks. map f $ [0..nLabels-1]
   where ones = konst 1.0 (1, repeatNo)
         zeros = konst 0.0 (1, repeatNo)
         f i | i == correctIndex = [ones]
             | otherwise = [zeros]
 
--- | Performs the supervised training that results in a linear readout.
+-- | Performs a supervised training that results in a linear readout.
 -- See https://en.wikipedia.org/wiki/Tikhonov_regularization
-ridgeRegression :: 
+ridgeRegression ::
   Double  -- ^ Regularization constant
   -> Matrix Double
-  -> Matrix Double 
-  -> Maybe (Matrix Double)
+  -> Matrix Double
+  -> Maybe Readout
 ridgeRegression μ tA tB = linearSolve oA oB
   where
     oA = (tA <> tr tA) + (scalar μ * ident (rows tA))
@@ -118,36 +147,39 @@ ridgeRegression μ tA tB = linearSolve oA oB
 
 -- | Winner-takes-all classification method
 winnerTakesAll
-  :: V.Storable a
-  => Matrix Double  -- ^ Transposed readout matrix
+  :: (V.Storable a, Eq a)
+  => Readout  -- ^ `Readout` matrix
   -> Vector a  -- ^ Vector of possible classes
   -> Classifier a  -- ^ `Classifier`
 winnerTakesAll readout klasses response = klasses V.! klass
   where klass = maxIndex $ scores readout response
 
 -- | Evaluate the network state (nonlinear response) according
--- to some readout matrix trW.
-scores :: Matrix Double -> Matrix Double -> Vector Double
+-- to some `Readout` matrix.
+scores
+  :: Readout  -- ^ `Readout` matrix
+  -> Matrix Double  -- ^ Network state
+  -> Vector Double
 scores trW response = evalScores
   where w = trW <> response
         -- Sum the elements in each row
         evalScores = w #> vector (replicate (cols w) 1.0)
 
 classify
-  :: V.Storable a
+  :: (V.Storable a, Eq a)
      => Matrix Double -> Vector a -> Classifier a
 classify = winnerTakesAll
 {-# SPECIALIZE classify
   :: Matrix Double -> Vector Int -> Classifier Int
   #-}
 
--- | Calculates the error rate in %
+-- | Error rate in %
 errorRate :: (Eq a, Fractional err) => [a] -> [a] -> err
 errorRate tgtLbls cLbls = 100 * fromIntegral errNo / fromIntegral (length tgtLbls)
   where errNo = length $ errors $ zip tgtLbls cLbls
 {-# SPECIALIZE errorRate :: [Int] → [Int] → Double #-}
 
--- | Returns the misclassified cases
+-- | Misclassified cases
 errors :: Eq a => [(a, a)] -> [(a, a)]
 errors = filter (uncurry (/=))
 {-# SPECIALIZE errors :: [(Int, Int)] -> [(Int, Int)] #-}
